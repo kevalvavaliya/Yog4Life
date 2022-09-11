15b6093fcd3d099ce4d98d1482da2d5572168488
harikanani <hkanani191@gmail.com>
2022-09-11 18:26:26 +0530

    updated my profile and added edit profile section

--- node_server/controllers/userController.js
+++ node_server/controllers/userController.js
@@ -68,4 +68,54 @@ module.exports = {
 			});
 		}
 	},
+
+	editProfile: async (req, res) => {
+		// edit user profile
+		try {
+			let user = await UsersModel.findOne({
+				_id: req.user.id,
+			});
+
+			if (!user) {
+				return res.status(400).json({
+					message: "User not found",
+					data: null,
+				});
+			}
+
+			if (req.body.username) {
+				user.username = req.body.username;
+			}
+
+			if (req.body.email) {
+				user.email = req.body.email;
+			}
+
+			if (req.body.bio) {
+				user.bio = req.body.bio;
+			}
+
+			if (req.body.profile_pic) {
+				user.profile_pic = req.body.profile_pic;
+			}
+
+			user = await user.save();
+
+			user.otp = "";
+
+			let users_blogs = await PostsModel.find({
+				author: req.user.id,
+			}).populate("author", ["username", "profile_pic"]);
+
+			return res.status(200).json({
+				message: "User profile updated successfully",
+				data: { user, users_blogs },
+			});
+		} catch (error) {
+			return res.status(500).json({
+				message: "Internal server error",
+				error: error.message,
+			});
+		}
+	},
 };
