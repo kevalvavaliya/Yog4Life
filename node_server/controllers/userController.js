4566da70e5bfdfddf941c291037a3b5100e2190f
harikanani <hkanani191@gmail.com>
2022-09-11 18:12:22 +0530

    check others profile

--- node_server/controllers/userController.js
+++ node_server/controllers/userController.js
@@ -12,12 +12,53 @@ module.exports = {
 
 			let users_blogs = await PostsModel.find({
 				author: req.user.id,
-			});
+			}).populate("author", ["username", "profile_pic"]);
+
+			if (!user) {
+				return res.status(400).json({
+					message: "User not found",
+					data: null,
+				});
+			}
 
 			return res.status(200).json({
 				message: "User profile fetched successfully",
 				data: {
 					user,
+					users_blogs,
+				},
+			});
+		} catch (error) {
+			return res.status(500).json({
+				message: "Internal server error",
+				error: error.message,
+			});
+		}
+	},
+
+	getProfileById: async (req, res) => {
+		try {
+			// get user profile
+			let user = await UsersModel.findOne({
+				_id: req.params.id,
+			}).select("-otp");
+
+			if (!user) {
+				return res.status(400).json({
+					message: "User not found",
+					data: null,
+				});
+			}
+
+			let users_blogs = await PostsModel.find({
+				author: req.params.id,
+			}).populate("author", ["username", "profile_pic"]);
+
+			return res.status(200).json({
+				message: "User profile fetched successfully",
+				data: {
+					user,
+					users_blogs,
 				},
 			});
 		} catch (error) {
